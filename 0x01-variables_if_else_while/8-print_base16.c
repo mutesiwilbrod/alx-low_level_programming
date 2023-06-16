1e7bd608301f1f7ba49ae7e09a8655c6d69181b5
mutesiwilbrod <willbrodmutesi@gmail.com>
2023-06-16 13:07:07 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a hexadecimal string
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
