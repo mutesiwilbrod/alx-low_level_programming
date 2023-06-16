7038e6a9716a4a3b445ab719913841f06a9d8668
mutesiwilbrod <willbrodmutesi@gmail.com>
2023-06-16 13:11:08 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a serie of numbers with commas
+  *
+  * Return: Always (Success);
+  */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
