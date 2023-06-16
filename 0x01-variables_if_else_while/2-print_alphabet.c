939b10a2dd40052aadc399ed4455f2ddce341e2e
mutesiwilbrod <willbrodmutesi@gmail.com>
2023-06-16 12:25:30 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for letter = 'a'; letter<= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
+
