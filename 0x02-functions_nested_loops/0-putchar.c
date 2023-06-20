c27f85475d81c4a683a2108f89877bd563cc21af
mutesiwilbrod <willbrodmutesi@gmail.com>
2023-06-20 12:17:57 -0700

    put chars

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - Entry point of the program
- *
- * Return: 0 on success
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	printf("_putchar\n");
+	puts("_putchar");
+
 	return (0);
 }
-
