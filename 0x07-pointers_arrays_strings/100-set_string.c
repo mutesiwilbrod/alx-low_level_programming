b3bd74910d023e0b6ac594affd3ba8ae70ae2bb4
mutesiwilbrod <willbrodmutesi@gmail.com>
2023-07-03 11:58:05 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* set_string - sets the value of apointer to a char
+* @s: pointer to pinter we need to set to
+* @to: string to set
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
