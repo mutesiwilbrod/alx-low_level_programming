d9a93b6ada47e1860c42e6d2a3a84340243d8d47
mutesiwilbrod <willbrodmutesi@gmail.com>
2023-07-18 12:34:19 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,24 +1,12 @@
 #include <stdio.h>
-<<<<<<< HEAD
 
 /**
- * main - generates file name
+ * main - prints the name of the file
  *
- * Return: All time 0 (success)
+ * Return: Always 0 (Success)
  */
-
-=======
-/**
-* main - prints the name of the file. *
-* Return: Always 0.
-*/
->>>>>>> 8ff92e5207b80fcc87e26e2ccfe02d1b45b515a5
 int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
-<<<<<<< HEAD
-
-=======
->>>>>>> 8ff92e5207b80fcc87e26e2ccfe02d1b45b515a5
 }
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
 #define ABS(x) ((x) < (0) ? -(x) : (x))
-=======
-#ifndef _FUNCTION_LIKE_MACRO_
-#define _FUNCTION_LIKE_MACRO_
-
-#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
->>>>>>> 8ff92e5207b80fcc87e26e2ccfe02d1b45b515a5
 
 #endif
